52667013d7c93c30ed00a5aa9ea11adcfc20bb3b
Samuel Humiwat <samuelhumiwat1312@gmail.com>
2026-04-01 21:31:03 +0800

    Adopt_A_Dog_App_Humiwat

---
+++ adopt_a_dog/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\samue\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\samue\wmad-306-1\adopt_a_dog"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ adopt_a_dog/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\samue\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "c:\\Users\\samue\\wmad-306-1\\adopt_a_dog" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\samue\\flutter\\flutter"
+  "PROJECT_DIR=c:\\Users\\samue\\wmad-306-1\\adopt_a_dog"
+  "FLUTTER_ROOT=C:\\Users\\samue\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=c:\\Users\\samue\\wmad-306-1\\adopt_a_dog\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=c:\\Users\\samue\\wmad-306-1\\adopt_a_dog"
+  "FLUTTER_TARGET=c:\\Users\\samue\\wmad-306-1\\adopt_a_dog\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNQ==,RkxVVFRFUl9DSEFOTkVMPVt1c2VyLWJyYW5jaF0=,RkxVVFRFUl9HSVRfVVJMPXVua25vd24gc291cmNl,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MmM5ZWIyMDczOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MDUyZjMxZDExNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=c:\\Users\\samue\\wmad-306-1\\adopt_a_dog\\.dart_tool\\package_config.json"
+)
