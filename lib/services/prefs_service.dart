616be8bc2357c45cc3c86f6766edeb204f21e5d1
dextersueno777 <dextersueno8080@gmail.com>
2026-04-14 16:33:30 +0800

    chore: save all changes before branch switch

--- lib/services/prefs_service.dart
+++ lib/services/prefs_service.dart
@@ -1,20 +1,49 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefsService {
-  static const _keyFavorite = 'favorite_breed';
+  static String _imageKey(String breedName) => 'favorite_image_$breedName';
 
-  Future<void> saveFavorite(String breedName) async {
+  Future<void> saveFavoriteWithImage(String breedName, String imageUrl) async {
+    await addFavorite(breedName);
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyFavorite, breedName);
+    await prefs.setString(_imageKey(breedName), imageUrl);
   }
 
-  Future<String?> loadFavorite() async {
+  Future<String?> getFavoriteImageUrl(String breedName) async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyFavorite);
+    return prefs.getString(_imageKey(breedName));
   }
 
-  Future<void> clearFavorite() async {
+  Future<void> removeFavoriteImageUrl(String breedName) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_keyFavorite);
+    await prefs.remove(_imageKey(breedName));
+  }
+
+  static const _keyFavorites = 'favorite_breeds';
+
+  Future<void> addFavorite(String breedName) async {
+    final prefs = await SharedPreferences.getInstance();
+    final favorites = prefs.getStringList(_keyFavorites) ?? [];
+    if (!favorites.contains(breedName)) {
+      favorites.add(breedName);
+      await prefs.setStringList(_keyFavorites, favorites);
+    }
+  }
+
+  Future<void> removeFavorite(String breedName) async {
+    final prefs = await SharedPreferences.getInstance();
+    final favorites = prefs.getStringList(_keyFavorites) ?? [];
+    favorites.remove(breedName);
+    await prefs.setStringList(_keyFavorites, favorites);
+  }
+
+  Future<List<String>> loadFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_keyFavorites) ?? [];
+  }
+
+  Future<void> clearFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_keyFavorites);
   }
 }
