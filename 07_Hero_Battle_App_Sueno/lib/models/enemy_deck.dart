6ffca3619fba00f5510e6219ecc2175a345d2f98
dextersueno777 <dextersueno8080@gmail.com>
2026-05-02 18:53:30 +0800

    Move project into 07_Hero_Battle_App_Sueno folder (clean PR)

--- 07_Hero_Battle_App_Sueno/lib/models/enemy_deck.dart
+++
@@ -1,8 +0,0 @@
-import 'hero_model.dart';
-
-class EnemyDeck {
-  final String name;
-  final List<HeroModel> heroes;
-
-  EnemyDeck({required this.name, required this.heroes});
-}
--- 07_Hero_Battle_App_Sueno/lib/services/api_service.dart
+++
@@ -1,8 +0,0 @@
-import '../models/hero_model.dart';
-import 'superhero_api_service.dart';
-
-class ApiService {
-  Future<List<HeroModel>> fetchHeroes() async {
-    return await SuperheroApiService().fetchAllHeroes();
-  }
-}
