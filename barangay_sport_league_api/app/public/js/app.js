c245da4d847feedb47b0e86eb8627dae3acaf761
Jaquidel <evadonardem@gmail.com>
2026-05-07 08:41:19 +0800

    Updated and fixed project

--- barangay_sport_league_api/app/public/js/app.js
+++ barangay_sport_league_api/app/public/js/app.js
@@ -1,4 +1,4 @@
-﻿window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
     var yearElement = document.getElementById('year');
     if (yearElement) {
         yearElement.textContent = new Date().getFullYear();
